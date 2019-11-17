137da5dbe0abd59de6a08c1e97111c5c313f4565
Nebil Muren <nebilmuren@gmail.com>
2019-11-17 15:21:18 -0400

    Book class added

--- Abstract_Classes/src/abstract_classes/Abstract_Classes.java
+++ Abstract_Classes/src/abstract_classes/Abstract_Classes.java
@@ -3,6 +3,8 @@ package abstract_classes;
 
 public class Abstract_Classes {
     
+    
+    
     public static void main(String[] args) {
         
     }
---
+++ Abstract_Classes/src/abstract_classes/Book.java
@@ -0,0 +1,14 @@
+package abstract_classes;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private int price;
+
+    public Book() {
+        title = "";
+        author = "";
+        price = 0;
+    }
+}
